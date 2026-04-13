16af40a014b2df1226ffaba0570addc458c64c86
Yakov Karpov <Yakov.Karpov@finonex.com>
2026-04-13 15:55:34 +0300

    Fix rainbow-delimiters on transient UI buffers.
    Restrict the plugin to normal file buffers so Noice and other temporary windows without a stable parser do not trigger nil parser errors during attach.

--- lazyvim/plugins.nix
+++ lazyvim/plugins.nix
@@ -168,6 +168,9 @@
         config = function()
           local rainbow = require("rainbow-delimiters")
           require("rainbow-delimiters.setup").setup({
+            condition = function(bufnr)
+              return vim.bo[bufnr].buftype == ""
+            end,
             strategy = {
               [""] = rainbow.strategy["global"],
             },
